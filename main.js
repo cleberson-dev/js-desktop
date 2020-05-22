3d4625379cbb95a742285ee3f836650c9557688a
Cleberson F R Junior <cleberson.dev@gmail.com>
2020-05-22 17:24:58 -0300

    Implementado experiência similar para aplicações macOS

--- main.js
+++ main.js
@@ -4,6 +4,20 @@ app
   .whenReady()
   .then(createWindow);
 
+app.on('window-all-closed', () => {
+  // Em macOS, é comum apps manterem ativos mesmo fechando as janelas.
+  if (process.platform === 'darwin') return;
+
+  app.quit();
+});
+
+app.on('activate', () => {
+  // Na ativação pelo ícone do app no docker, é comum recriar a janela quando não houverem outras
+  if (BrowserWindow.getAllWindows().length > 0) return;
+  
+  createWindow();
+})
+
 function createWindow() {
   const win = new BrowserWindow({ 
     width: 600, height: 480, 
